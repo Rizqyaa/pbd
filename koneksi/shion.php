b45e408077cef67be1c20a668d8ce11cc7f0710a
kazuzha <98564529+Rizqyaa@users.noreply.github.com>
2022-09-26 19:07:15 -0700

    Update shion.php

--- koneksi/shion.php
+++ koneksi/shion.php
@@ -6,7 +6,4 @@ $password = $_POST['password'];
 $koneksi = new PDO('mysql:host=localhost;dbname=mixx', 'root', '');
 $query = $koneksi->query("select * from login where nama='$nama' AND password='$password'");
 $data = $query->fetch();
-if($query->rowCount() > 0){
-
     header("Location:index.php");
-}
